37ae4c0b78ba752cf89e1e63a4f78c819d7ae5cf
Александр Дунаевский <yaltasashay@gmail.com>
2018-12-22 01:04:21 +0300

    Add new actions on Photo component

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,11 +1,226 @@
-import React from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import {
+  Image,
+  Segment,
+  Header,
+  Divider,
+  Grid,
+  Button,
+  Card,
+  Icon
+} from "semantic-ui-react";
+import { toastr } from "react-redux-toastr";
+import Dropzone from "react-dropzone";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { uploadProfileImage, deletePhoto, setMainPhoto } from "../userActions";
 
-const PhotosPage = () => {
-  return (
-    <div>
-      <h1>PhotosPage</h1>
-    </div>
-  )
+const query = ({ auth }) => {
+  return [
+    {
+      collection: "users",
+      doc: auth.uid,
+      subcollections: [{ collection: "photos" }],
+      storeAs: "photos"
+    }
+  ];
+};
+
+const actions = {
+  uploadProfileImage,
+  deletePhoto,
+  setMainPhoto
+};
+
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos,
+  loading: state.async.loading
+});
+
+class PhotosPage extends Component {
+  state = {
+    files: [],
+    fileName: "",
+    cropResult: null,
+    image: {}
+  };
+
+  uploadImage = async () => {
+    try {
+      await this.props.uploadProfileImage(
+        this.state.image,
+        this.state.fileName
+      );
+      this.cancelCrop();
+      toastr.success("Успех!", "Фото было обновленно👍");
+    } catch (error) {
+      toastr.error("Упс...", error.message);
+    }
+  };
+
+  handlePhotoDelete = photo => async () => {
+    try {
+      this.props.deletePhoto(photo);
+    } catch (error) {
+      toastr.error("Упс...", error.message);
+    }
+  };
+
+  handeSetMainPhoto = photo => async () => {
+    try {
+      this.props.setMainPhoto(photo);
+    } catch (error) {
+      toastr.error("Упс...", error.message);
+    }
+  };
+
+  cancelCrop = () => {
+    this.setState({
+      files: [],
+      image: {}
+    });
+  };
+
+  cropImage = () => {
+    if (typeof this.refs.cropper.getCroppedCanvas() === "undefined") {
+      return;
+    }
+
+    this.refs.cropper.getCroppedCanvas().toBlob(blob => {
+      let imageUrl = URL.createObjectURL(blob);
+      this.setState({
+        cropResult: imageUrl,
+        image: blob
+      });
+    }, "image/jpeg");
+  };
+
+  onDrop = files => {
+    this.setState({
+      files,
+      fileName: files[0].name
+    });
+  };
+
+  render() {
+    const { photos, profile, loading } = this.props;
+    let filterPhotos;
+    if (photos) {
+      filterPhotos = photos.filter(photo => {
+        return photo.url !== profile.photoURL;
+      });
+    }
+
+    return (
+      <Segment>
+        <Header dividing size="large" content="Мои фото" />
+        <Grid>
+          <Grid.Row />
+          <Grid.Column width={4}>
+            <Header color="teal" sub content="Шаг 1 - Добавить фото" />
+            <Dropzone onDrop={this.onDrop} multiple={false}>
+              <div style={{ padding: "15px", textAlign: "center" }}>
+                <Icon name="upload" size="huge" />
+                <Header content="Перетащите фото сюда или нажмите добавить." />
+              </div>
+            </Dropzone>
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header
+              sub
+              color="teal"
+              content="Шаг 2 - Изменить размер изображения"
+            />
+            {this.state.files[0] && (
+              <Cropper
+                style={{ height: 200, width: "100%" }}
+                ref="cropper"
+                src={this.state.files[0].preview}
+                aspectRatio={1}
+                dragMode="move"
+                guides={false}
+                scalable={true}
+                cropBoxMovable={true}
+                cropBoxResizable={true}
+                crop={this.cropImage}
+              />
+            )}
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color="teal" content="Шаг 3 - Просмотр и загрузка" />
+            {this.state.files[0] && (
+              <div>
+                <Image
+                  style={{ minHeight: "200px", minWidth: "200px" }}
+                  src={this.state.cropResult}
+                />
+                <Button.Group>
+                  <Button
+                    loading={loading}
+                    onClick={this.uploadImage}
+                    style={{ width: "109px" }}
+                    positive
+                    icon="check"
+                  />
+                  <Button
+                    disabled={loading}
+                    onClick={this.cancelCrop}
+                    style={{ width: "109px" }}
+                    negative
+                    icon="close"
+                  />
+                </Button.Group>
+              </div>
+            )}
+          </Grid.Column>
+        </Grid>
+
+        <Divider />
+        <Header sub color="teal" content="Все фото" />
+
+        <Card.Group itemsPerRow={5}>
+          <Card>
+            <Image src={profile.photoURL || "/assets/user.png"} />
+            <Button positive>Основное фото</Button>
+          </Card>
+          {photos &&
+            filterPhotos.map(photo => (
+              <Card key={photo.id}>
+                <Image src={photo.url} />
+                <div className="ui two buttons">
+                  <Button
+                    onClick={this.handeSetMainPhoto(photo)}
+                    basic
+                    color="green"
+                  >
+                    Выбрать
+                  </Button>
+                  <Button
+                    onClick={this.handlePhotoDelete(photo)}
+                    basic
+                    icon="trash"
+                    color="red"
+                  />
+                </div>
+              </Card>
+            ))}
+        </Card.Group>
+      </Segment>
+    );
+  }
 }
 
-export default PhotosPage
+export default compose(
+  connect(
+    mapState,
+    actions
+  ),
+  firestoreConnect(auth => query(auth))
+)(PhotosPage);
--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,5 +1,11 @@
 import moment from "moment";
 import { toastr } from "react-redux-toastr";
+import cuid from "cuid";
+import {
+  asyncActionStart,
+  asyncActionFinished,
+  asyncActionError
+} from "../async/asyncActions";
 
 export const updateProfile = user => async (
   dispatch,
@@ -17,5 +23,95 @@ export const updateProfile = user => async (
     toastr.success("Успех!", "Профиль был изменен 👍");
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_image`;
+  const imageName = cuid();
+  const options = {
+    name: imageName
+  };
+
+  try {
+    dispatch(asyncActionStart());
+    // Загрузить файл в firebase storage
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // Получить URL картинки
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+    // Получить userdoc
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    // Проверить есть ли у пользователя фото если нет загрузить
+    if (!userDoc.data().photoURL) {
+      await firebase.updateProfile({
+        photoURL: downloadURL
+      });
+      await user.updateProfile({
+        photoURL: downloadURL
+      });
+    }
+    // Добавить новое фото в коллекцию
+    await firestore.add(
+      {
+        collection: "users",
+        doc: user.uid,
+        subcollections: [{ collection: "photos" }]
+      },
+      {
+        name: imageName,
+        url: downloadURL
+      }
+    );
+    dispatch(asyncActionFinished());
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
+    throw new Error("Houston, we have a problem with upload");
+  }
+};
+
+export const deletePhoto = photo => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+
+  try {
+    await firebase.deleteFile(`${user.uid}/user_image/${photo.name}`);
+    await firestore.delete({
+      collection: "users",
+      doc: user.uid,
+      subcollections: [{ collection: "photos", doc: photo.id }]
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Houston, we have a problem with delete");
+  }
+};
+
+export const setMainPhoto = photo => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+
+  try {
+    return await firebase.updateProfile({
+      photoURL: photo.url
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Houston, we have a problem set photo");
   }
 };
